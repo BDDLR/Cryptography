ba85a1b1dd424ee1c468c16e67b2319640f95a97
Bryan2 <Bryan2@192.168.1.69>
2018-08-09 00:36:28 -0500

    Creating java class that implements AES encryption

---
+++ 1erParcial/Tareas/FirstCryptographyFunction/src/firstcryptographyfunction/AESencryption.java
@@ -0,0 +1,47 @@
+
+package firstcryptographyfunction;
+
+
+import java.security.Key;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.BASE64Encoder;
+import sun.misc.BASE64Decoder;
+
+public class AESencryption {
+    private static final String ALGORITHM = "AES";      //Algorithm to use
+    private byte[] keyValue;                         //Secret key to use
+
+    //Constructor that receives the key to use
+    public AESencryption(String key) {
+        keyValue = key.getBytes();
+    }
+    
+    public String encryptData(String data) throws Exception{
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(data.getBytes());
+        String encryptedValue = new BASE64Encoder().encode(encVal);
+        return encryptedValue;
+    }
+    
+    public String decryptData(String encryptedData) throws Exception{
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedData);
+        byte[] decValue = c.doFinal(decordedValue);
+        String decryptedValue = new String(decValue);
+        
+        return decryptedValue;
+    }
+    
+    private Key generateKey() throws Exception{
+        Key key = new SecretKeySpec(keyValue, ALGORITHM);
+        return key;
+    }
+    
+}
--- 1erParcial/Tareas/FirstCryptographyFunction/src/firstcryptographyfunction/FirstCryptographyFunction.java
+++ 1erParcial/Tareas/FirstCryptographyFunction/src/firstcryptographyfunction/FirstCryptographyFunction.java
@@ -3,8 +3,15 @@ package firstcryptographyfunction;
 
 public class FirstCryptographyFunction {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
+        AESencryption aes = new AESencryption("SuperLlaveBryan1");
+        
+        String encData = aes.encryptData("Hola Mundo");
+        System.out.println("ENCRYPTED: " + encData);
+        
+        String decData = aes.decryptData(encData);
+        System.out.println("DECRYPTED: " + decData);
     }
     
 }
