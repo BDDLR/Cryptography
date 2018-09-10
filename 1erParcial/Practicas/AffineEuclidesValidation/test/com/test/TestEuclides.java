236e41f357e31146f4f7f436c037660322d8d5e8
Bryan2 <Bryan2@192.168.1.69>
2018-09-10 00:36:30 -0500

    Practica euclides

---
+++ 1erParcial/Practicas/AffineEuclidesValidation/test/com/test/TestEuclides.java
@@ -0,0 +1,11 @@
+
+package com.test;
+
+import com.algorithm.EuclidesValidation;
+
+public class TestEuclides {
+    public static void main(String[] args) {
+        EuclidesValidation ev = new EuclidesValidation(21,10,26);
+        ev.euclidesExtendido();
+    }
+}
