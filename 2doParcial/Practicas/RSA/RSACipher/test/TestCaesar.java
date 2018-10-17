d762df71b8c23a7abac42265ac1267c8c19ea0ca
Bryan2 <Bryan2@192.168.1.69>
2018-10-17 08:55:19 -0500

    Inicio practica RSA

---
+++ 2doParcial/Practicas/RSA/RSACipher/test/TestCaesar.java
@@ -0,0 +1,11 @@
+
+import com.rsa.AffineCipher;
+
+public class TestCaesar {
+
+    public static void main(String[] args) {
+        //ShiftCipher c = new AffineCipher("DEFGHIJKLMNOPQRSTUVWXYZABC", 3);
+        //System.out.println(c.decrypt());
+    }
+
+}
--- 2doParcial/Practicas/RSA/WebSite/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    console.log("Holis crayolis");
-    console.log(elems[0]);
-    var instances = M.Materialbox.init(elems, options);
-
-  });
