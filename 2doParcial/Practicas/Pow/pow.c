f29958bdced575a6f9afe7394fa7906349c7c8e6
Bryan Dominguez <bryan.dominguez.delarosa@gmail.com>
2018-10-21 23:10:28 -0500

    Ejercicio de exponenciacion finalizado

--- 2doParcial/Practicas/Pow/pow.c
+++ 2doParcial/Practicas/Pow/pow.c
@@ -3,15 +3,24 @@
 
 int main(int argc, char const *argv[])
 {
-	long b = 85;
-	long e = 5;
-	long n = 10;
+	long b;
+	long e;
+	long n;
 	long c = 1;
+
+	printf("Type the base: \n");
+	scanf("%li", &b);
+
+	printf("Type the exponent: \n");
+	scanf("%li", &e);
+
+	printf("Type the ring: \n");
+	scanf("%li", &n);
 
 	for (int i = 0; i < e; i++)
 		c=(b*c)%n;
 
-	printf("%li\n", c);
+	printf("The result is: %li\n", c);
 
 	return 0;
 }
